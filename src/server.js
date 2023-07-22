08fd79a14d59b00aeb453c016ff8498fdd23c0e9
lathief <shadowmaster241099@yahoo.co.id>
2023-07-22 17:51:01 +0700

    feat: initialization user and auth api

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 const Hapi = require('@hapi/hapi');
 const songs = require('./api/song');
 const albums = require('./api/album');
+const users = require('./api/users');
+const authentication = require('./api/authentication');
 const SongsService = require('./services/postgres/songsService');
 const SongsValidator = require('./validator/songs');
 const AlbumsService = require('./services/postgres/albumsService');
+const UsersValidator = require('./validator/users');
+const UsersService = require('./services/postgres/usersService');
+const AuthenticationValidator = require('./validator/authentication');
+const AuthenticationService = require('./services/postgres/authenticationService');
 const AlbumValidator = require('./validator/albums');
 const ClientError = require('./exception/client-error');
+
+const TokenManager = require('./tokenize/tokenManager');
 
 require('dotenv').config();
 
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationService();
   const server = Hapi.server({
     port: process.env.SERVER_PORT || 8080,
     host: process.env.SERVER_HOST || 'localhost',
@@ -23,6 +33,15 @@ const init = async () => {
   });
 
   await server.register([
+    {
+      plugin: authentication,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationValidator,
+      },
+    },
     {
       plugin: songs,
       options: {
@@ -37,10 +56,18 @@ const init = async () => {
         validator: AlbumValidator,
       },
     },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
   ]);
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
     if (response instanceof Error) {
+      console.log(response);
       if (response instanceof ClientError) {
         const newResponse = h.response({
           status: 'fail',
