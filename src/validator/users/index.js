97e89dcb6ec44b46a0e535bf64b74d502d3c2443
lathief <shadowmaster241099@yahoo.co.id>
2023-07-22 17:49:06 +0700

    feat: validator for user and auth

---
+++ src/validator/users/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exception/invariant-error');
+const { UserPayloadSchema } = require('./schema');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validationResult = UserPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
